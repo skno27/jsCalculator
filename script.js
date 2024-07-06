dcc012871220af6c4bd027b79438abd2fb4aaf4c
Shon Abdullah <Abdullah@Skeno-Abdu-Rahman.local>
2024-07-06 03:14:10 -0500

    pulled code from previous application. adding improved styling

---
+++ script.js
@@ -0,0 +1,60 @@
+const resultText = document.getElementsByClassName("result-text")[0];
+
+let storedValue = 0;
+let operator = null;
+
+function pressNumber(number) {
+  if (number === ".") {
+    if (resultText.innerHTML.length === 0 || resultText.innerHTML.includes("."))
+      return;
+  }
+  resultText.innerHTML += number.toString();
+}
+
+function canOperate() {
+  return resultText.innerHTML.length >= 1;
+}
+
+function operate(number) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = storedValue + number;
+      break;
+
+    case "-":
+      result = storedValue - number;
+      break;
+
+    case "x":
+      result = storedValue * number;
+      break;
+
+    case "/":
+      result = storedValue / number;
+      break;
+  }
+
+  resultText.innerHTML = result;
+  storedValue = null;
+  operator = null;
+}
+
+function pressOperator(operation) {
+  const number = parseFloat(resultText.innerHTML);
+
+  if (!canOperate()) return;
+
+  if (operation === "=" && storedValue !== null) {
+    return operate(number);
+  } else if (operation === "c") {
+    storedValue = null;
+    operator = null;
+  } else {
+    operator = operation;
+    storedValue = number;
+  }
+
+  resultText.innerHTML = "";
+}
